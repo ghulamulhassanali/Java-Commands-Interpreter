7d72eab340d223a731543303ec1b9abc8dd514f3
ghulamulhassanali <ghulamulhassanali@gmail.com>
2015-02-26 23:49:51 +0500

    Create UnitTest.java

---
+++ UnitTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pk.edu.nust.seecs.bscs2.advancedprogramming.lab2;
+
+/**
+ *
+ * @author test1
+ */
+public class UnitTests {
+    
+     public static void main(String[] args) {
+         //Read commands from a file
+         GetCommands getCommands = new GetCommands();
+         getCommands.readCommands();
+         
+         
+         
+         //check for any syntax errors
+         //executed these commands and display the result         
+     }        
+}
